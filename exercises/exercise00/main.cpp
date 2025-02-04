2294ae5f08819717126a080cbdedb4ab7d28e651
William Skou Heidemann <williamskou2227@gmail.com>
2025-02-04 11:39:16 +0100

    at 00.5.5

--- exercises/exercise00/main.cpp
+++ exercises/exercise00/main.cpp
@@ -6,18 +6,19 @@
 #include <ituGL/geometry/VertexBufferObject.h>
 #include <ituGL/geometry/VertexArrayObject.h>
 #include <ituGL/geometry/VertexAttribute.h>
+#include <ituGL/geometry/ElementBufferObject.h>
 
 #include <iostream>
 
 unsigned int BuildShaderProgram();
-void processInput(GLFWwindow* window);
+
+void processInput(GLFWwindow *window);
 
 // settings
 const unsigned int SCR_WIDTH = 800;
-const unsigned int SCR_HEIGHT = 600;
+const unsigned int SCR_HEIGHT = 800;
 
-int main()
-{
+int main() {
     DeviceGL device;
 
     // glfw window creation
@@ -25,8 +26,7 @@ int main()
 
     Window window(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL");
 
-    if (!window.IsValid())
-    {
+    if (!window.IsValid()) {
         std::cout << "Failed to create GLFW window" << std::endl;
         return -1;
     }
@@ -35,8 +35,7 @@ int main()
 
     // glad: load all OpenGL function pointers
     // ---------------------------------------
-    if (!device.IsReady())
-    {
+    if (!device.IsReady()) {
         std::cout << "Failed to initialize GLAD" << std::endl;
         return -1;
     }
@@ -46,31 +45,46 @@ int main()
     // set up vertex data (and buffer(s)) and configure vertex attributes
     // ------------------------------------------------------------------
     float vertices[] = {
-        -0.5f, -0.5f, 0.0f, // left  
-         0.5f, -0.5f, 0.0f, // right 
-         0.0f,  0.5f, 0.0f  // top   
+        -0.5f, -0.5f, 0.0f, // bottom-left
+        0.5f, -0.5f, 0.0f, // bottom-right
+        0.5f, 0.5f, 0.0f, // top-right
+        -0.5f, 0.5f, 0.0f // top-left
+    };
+
+    unsigned int indices[] = {
+        0, 1, 2,
+        2, 3, 0
     };
 
     unsigned int VBO, VAO;
     VertexBufferObject vbo;
     VertexArrayObject vao;
-    glGenVertexArrays(1, &VAO);
-    glGenBuffers(1, &VBO);
+    ElementBufferObject ebo;
+
+    constexpr int indicesCount = sizeof(indices) / sizeof(unsigned int);
+    constexpr int verticesCount = sizeof(vertices) / sizeof(float);
+
     // bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
-    glBindVertexArray(VAO);
+    vao.Bind();
 
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    vbo.Bind();
+    vbo.AllocateData<float>(std::span(vertices,  verticesCount));
 
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-    glEnableVertexAttribArray(0);
+    ebo.Bind();
+    ebo.AllocateData(std::span(indices, indicesCount));
+
+
+    VertexAttribute position(Data::Type::Float, 3);
+    vao.SetAttribute(0, position, 0);
 
     // note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    VertexBufferObject::Unbind();
 
     // You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
     // VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
-    glBindVertexArray(0);
+    VertexArrayObject::Unbind();
+
+    ElementBufferObject::Unbind();
 
 
     // uncomment this call to draw in wireframe polygons.
@@ -78,19 +92,25 @@ int main()
 
     // render loop
     // -----------
-    while (!window.ShouldClose())
-    {
+    float time = 0.5;
+    while (!window.ShouldClose()) {
         processInput(window.GetInternalWindow());
 
         // render
         // ------
-        device.Clear(0.2f, 0.3f, 0.3f, 1.0f);
+        device.Clear(0.2f, 0.1f, 0.8f, 1.0f);
+        time += 0.001f;
+        vertices[3] = time;
+        vbo.UpdateData(std::span(vertices, verticesCount));
 
         // draw our first triangle
         glUseProgram(shaderProgram);
-        glBindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
-        glDrawArrays(GL_TRIANGLES, 0, 3);
-        // glBindVertexArray(0); // no need to unbind it every time 
+        vao.Bind();
+        // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
+        // glDrawArrays(GL_TRIANGLES, 0, 6);
+        glDrawElements(GL_TRIANGLES, indicesCount, GL_UNSIGNED_INT, 0);
+
+        // glBindVertexArray(0); // no need to unbind it every time
 
         // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
         // -------------------------------------------------------------------------------
@@ -100,36 +120,31 @@ int main()
 
     // optional: de-allocate all resources once they've outlived their purpose:
     // ------------------------------------------------------------------------
-    glDeleteVertexArrays(1, &VAO);
-    glDeleteBuffers(1, &VBO);
     glDeleteProgram(shaderProgram);
-
 
     return 0;
 }
 
 // process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
 // ---------------------------------------------------------------------------------------------------------
-void processInput(GLFWwindow* window)
-{
+void processInput(GLFWwindow *window) {
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, true);
 }
 
-unsigned int BuildShaderProgram()
-{
-    const char* vertexShaderSource = "#version 330 core\n"
-        "layout (location = 0) in vec3 aPos;\n"
-        "void main()\n"
-        "{\n"
-        "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-        "}\0";
-    const char* fragmentShaderSource = "#version 330 core\n"
-        "out vec4 FragColor;\n"
-        "void main()\n"
-        "{\n"
-        "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
-        "}\n\0";
+unsigned int BuildShaderProgram() {
+    const char *vertexShaderSource = "#version 330 core\n"
+            "layout (location = 0) in vec3 aPos;\n"
+            "void main()\n"
+            "{\n"
+            "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+            "}\0";
+    const char *fragmentShaderSource = "#version 330 core\n"
+            "out vec4 FragColor;\n"
+            "void main()\n"
+            "{\n"
+            "   FragColor = vec4(0.8f, 0.1f, 0.4f, 1.0f);\n"
+            "}\n\0";
 
     // build and compile our shader program
     // ------------------------------------
@@ -141,8 +156,7 @@ unsigned int BuildShaderProgram()
     int success;
     char infoLog[512];
     glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
-    if (!success)
-    {
+    if (!success) {
         glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
         std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
     }
@@ -152,8 +166,7 @@ unsigned int BuildShaderProgram()
     glCompileShader(fragmentShader);
     // check for shader compile errors
     glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
-    if (!success)
-    {
+    if (!success) {
         glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
         std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
     }
