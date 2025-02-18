b0f982a696147ccef3b581ddf1c6b4496aef8209
William Skou Heidemann <williamskou2227@gmail.com>
2025-02-18 17:35:39 +0100

    completed 03.1

--- exercises/exercise03/GearsApplication.cpp
+++ exercises/exercise03/GearsApplication.cpp
@@ -15,6 +15,7 @@
 GearsApplication::GearsApplication()
     : Application(1024, 1024, "Gears demo")
     , m_colorUniform(-1)
+    , m_worldMatrixUniform(0)
 {
 }
 
@@ -53,7 +54,8 @@ void GearsApplication::Render()
 
     // (todo) 03.1: Draw large gear at the center
     glm::mat4 centerGearMatrix(1.0f);
-    DrawGear(m_largeGear, centerGearMatrix, Color(1.0f, 1.0f, 1.0f));
+    glm::mat4 rotationMatrix = glm::rotate(GetCurrentTime(), glm::vec3(0.0f, 0.0f, -1.0f));
+    DrawGear(m_largeGear, rotationMatrix * centerGearMatrix, Color(1.0f, 1.0f, 1.0f));
 
     // (todo) 03.2: Draw medium gear to the right
 
@@ -96,7 +98,7 @@ void GearsApplication::InitializeShaders()
     m_colorUniform = m_shaderProgram.GetUniformLocation("Color");
 
     // (todo) 03.1: Find the WorldMatrix uniform location
-
+    m_worldMatrixUniform = m_shaderProgram.GetUniformLocation("WorldMatrix");
 
     // (todo) 03.5: Find the ViewProjMatrix uniform location
 
@@ -109,7 +111,7 @@ void GearsApplication::DrawGear(const Mesh& mesh, const glm::mat4& worldMatrix, 
     m_shaderProgram.SetUniform(m_colorUniform, static_cast<glm::vec3>(color));
 
     // (todo) 03.1: Set the value of the WorldMatrix uniform
-
+    m_shaderProgram.SetUniform(m_worldMatrixUniform, worldMatrix);
 
     mesh.DrawSubmesh(0);
 }
--- exercises/exercise03/shaders/basic.vert
+++ exercises/exercise03/shaders/basic.vert
@@ -7,9 +7,11 @@ layout (location = 1) in vec3 VertexNormal;
 
 
 // (todo) 03.X: Add the uniforms here
-
+uniform mat4 WorldMatrix = mat4(1.0f);
 
 void main()
 {
-	gl_Position = vec4(VertexPosition, 1.0);
+	vec4 localPosition = vec4(VertexPosition, 1.0);
+	vec4 worldPosition = WorldMatrix * localPosition;
+	gl_Position = worldPosition;
 }
